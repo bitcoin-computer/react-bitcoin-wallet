f43a941c4186a47c22aece7489300238328ef66d
Clemens Ley <ley.clemens@gmail.com>
2019-10-17 17:22:40 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Computer from 'bitcoin-computer'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [computer] = useState(new Computer({ seed: 'describe install ostrich blast region era course junior feed acoustic galaxy annual'}))
+  const [balance, setBalance] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [to, setTo] = useState('')
+
+  useEffect(async () => {
+    setBalance(await computer.db.wallet.getBalance())
+  }, [computer.db.wallet])
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault()
+    const { txId } = await computer.db.wallet.send(parseInt(amount), to)
+    alert(`Sent ${amount} to ${to} in tx with id\n${txId}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Wallet</h2>
+      <b>Address</b>&nbsp;{computer.db.wallet.getAddress().toString()}<br />
+      <b>Balance</b>&nbsp;{balance}
+
+      <h3>Send</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Amount&nbsp;
+          <input type="number" value={amount} onChange={e => setAmount(e.target.value)}>
+          </input>
+        </label><br />
+        <label>
+          To&nbsp;
+          <input type="string" value={to} onChange={e => setTo(e.target.value)}>
+          </input>
+        </label><br />
+        <input type="submit" value="Send Bitcoin"></input>
+      </form>
     </div>
   );
 }
