5d39e41a1fca719f6c1444b205e77408de8f4081
Clemens Ley <ley.clemens@gmail.com>
2020-08-19 20:37:32 -0700

    updated to latest version of Bitcoin Computer

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-A simple Bitcoin Wallet built using create-react-app. You can to make a deposit into the wallet by sending Bitcoin to the address shown. The wallet displays your balance and allows you to send Bitcoins to a friend. Build using [Bitcoin|Computer](https://bitcoin-computer.gitbook.io/docs/).
+# A Bitcoin Wallet in Reeact
+
+A simple Bitcoin Wallet built using [Create React App](https://create-react-app.dev/). You can to make a deposit into the wallet by sending Bitcoin to the address shown. The wallet displays your balance and allows you to send Bitcoins to a friend. Build using [Bitcoin|Computer](https://bitcoin-computer.gitbook.io/docs/).
 
 <img src="https://i.ibb.co/pbMjShG/Screen-Shot-2019-10-17-at-17-01-12.png" alt="drawing" width="400"/>
+
+## Start the App
+
+To run the code clone the project in a new folder, run ``npm install`` and start the app using
+
+````
+npm start
+````
+
+To run the tests run
+
+````
+npm test
+````
+
+## More Docs and Video
 
 
 You can find more information in the [docs](https://bitcoin-computer.gitbook.io/docs/). Also check out the corresponding [Youtube tutorial](https://www.youtube.com/watch?v=vcjzIFjt3VY).
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import Computer from 'bitcoin-computer'
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [computer] = useState(new Computer({ seed: 'describe install ostrich blast region era course junior feed acoustic galaxy annual'}))
+  const [computer] = useState(new Computer({
+    seed: 'describe install ostrich blast region era course junior feed acoustic galaxy annual',
+    chain: 'BSV'
+  }))
   const [balance, setBalance] = useState(0)
   const [amount, setAmount] = useState(0)
   const [to, setTo] = useState('')
@@ -15,15 +18,18 @@ function App() {
 
   const handleSubmit = async (evt) => {
     evt.preventDefault()
-    const { txId } = await computer.db.wallet.send(parseInt(amount), to)
-    alert(`Sent ${amount} to ${to} in tx with id\n${txId}`)
+    const txId = await computer.db.wallet.send(parseInt(amount * 1e8, 10), to)
+    const message = `Sent\n${amount}\n\nTo\n${to}\n\nTransaction id\n${txId}`
+    console.log(message)
+    alert(message)
   }
 
   return (
     <div className="App">
       <h2>Wallet</h2>
       <b>Address</b>&nbsp;{computer.db.wallet.getAddress().toString()}<br />
-      <b>Balance</b>&nbsp;{balance}
+      <b>Public Key</b>&nbsp;{computer.db.wallet.getPublicKey().toString()}<br />
+      <b>Balance</b>&nbsp;{balance/1e8} {computer.db.wallet.restClient.chain}
 
       <h3>Send</h3>
       <form onSubmit={handleSubmit}>
@@ -40,7 +46,7 @@ function App() {
         <input type="submit" value="Send Bitcoin"></input>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
